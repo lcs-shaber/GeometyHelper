3ffe94a0ac60f8f37dea9814a1656b0699acc0e6
Sophie Haber <SHaber@lcs.on.ca>
2024-01-15 16:08:34 -0500

    Organized project by adding Views and Model groups and a new view file named CircleView.

---
+++ GeometyHelper/Views/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  GeometyHelper
+//
+//  Created by Sophie Haber on 15.01.24.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
