003c631dd7479db0f1e189355f86bf03941883a5
Sophie Haber <SHaber@lcs.on.ca>
2024-01-15 16:35:39 -0500

    I added all the other values

--- GeometyHelper/Views/CircleView.swift
+++ GeometyHelper/Views/CircleView.swift
@@ -16,14 +16,44 @@ struct CircleView: View {
 
     var body: some View {
         VStack{
-            Text("\(currentCircle.radius)")
-                .font(.largeTitle)
             
+            HStack{
+                
+                Text("0")
+                Slider(value: $currentCircle.radius, in: 0...100)
+               Text("100")
+            }
             
-            Slider(value: $currentCircle.radius, in: 0...100)
-            
-            Text("\(currentCircle.diameter)")
-                .font(.largeTitle)
+            HStack{
+                VStack{
+                
+                    Text("Radius")
+                        .bold()
+                        .padding(.top)
+                    Text("\(currentCircle.radius)")
+                        .padding(.bottom)
+                    
+                    
+                    Text("Diameter")
+                        .bold()
+                    Text("\(currentCircle.diameter)")
+                       
+                    Text("Perimeter")
+                        .bold()
+                        .padding(.top)
+                    Text("\(currentCircle.perimeter)")
+                    
+                    Text("Area")
+                        .bold()
+                        .padding(.top)
+                    Text("(square units)")
+                        .font(Font.system(size: 10))
+                    Text("\(currentCircle.area)")
+                    
+                        .padding(.horizontal)
+                }
+                Spacer()
+            }
         }
         .padding()
     }
