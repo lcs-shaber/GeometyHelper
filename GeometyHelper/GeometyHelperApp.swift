0a50244baf39d0c6779f0dcf6a433c58ad250726
Sophie Haber <SHaber@lcs.on.ca>
2024-01-15 16:02:30 -0500

    Initial Commit

---
+++ GeometyHelper/GeometyHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometyHelperApp.swift
+//  GeometyHelper
+//
+//  Created by Sophie Haber on 15.01.24.
+//
+
+import SwiftUI
+
+@main
+struct GeometyHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
